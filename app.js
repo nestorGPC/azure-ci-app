e092b1e43eee05e06817cf79582fcb1da7740a1e
nestorGPC <palaciosnestor733@gmail.com>
2025-07-12 17:25:34 -0600

    Agregada ruta raíz /

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('¡Hola desde Azure Web App!');
+});
+
 app.get('/api/status', (req, res) => {
   res.json({ estado: 'ok' });
 });
